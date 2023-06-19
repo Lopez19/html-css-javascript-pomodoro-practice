56feb6c8ccc30429e27ab12c19f40562898adfea
Lopez19 <horaciolopm_2001@hotmail.com>
2023-06-18 21:02:11 -0500

    :tada: Primera carga de codigo

---
+++ main.js
@@ -0,0 +1,70 @@
+const TIMER = document.getElementById("pomodoro");
+const CONTAINER = document.getElementById("container");
+const SPANTXT = document.getElementById("txtModo");
+
+// Estructura del pomodoro:
+// 4 sesiones de 25 min,
+// 5 min de break y al completar las 4 sesiones
+// un descanso largo de 15 min
+const MINUTOS_DE_TRABAJO = 0.1; //.1 = 0.6s prueba
+const MINUTOS_DE_DESCANSO = 0.2;
+const MINUTOS_DE_DESCANSO_LARGO = 0.3; //18s
+const SESIONES = 4;
+
+let contadorDeSesiones = 1;
+let segundosTotales = MINUTOS_DE_TRABAJO * 60;
+
+//Puede ser "estudio, etc..."
+let modoTimer = "trabajo";
+
+// Funcion de la cuenta regresiva
+const cuentaRegresiva = () => {
+  // Minutos del contador
+  let minutosEnteros = Math.floor(segundosTotales / 60);
+  // Segundos del contador
+  let segundosEnteros = segundosTotales % 60;
+
+  // Añadimos un 0 al inicio de los segunfos si es < 0 -> 25: "0"+0 = 25:00
+  if (segundosEnteros < 10) {
+    segundosEnteros = "0" + segundosEnteros;
+  }
+
+  // Imprimimos el contador
+  TIMER.innerHTML = `${minutosEnteros}:${segundosEnteros} | ${contadorDeSesiones}/${SESIONES}`;
+  segundosTotales--;
+
+  //   Cambio de modo y añadimos la clase correspondiente
+  if (modoTimer === "trabajo") {
+    addAndRemoveClases("contador__trabajo", "contador__descanso");
+    SPANTXT.innerHTML = modoTimer;
+
+    if (segundosTotales < 0) {
+      if (contadorDeSesiones < SESIONES) {
+        contadorDeSesiones++;
+        modoTimer = "descanso";
+        segundosTotales = MINUTOS_DE_DESCANSO * 60;
+      } else {
+        modoTimer = "largo 15M";
+        segundosTotales = MINUTOS_DE_DESCANSO_LARGO * 60;
+        contadorDeSesiones = 1;
+      }
+    }
+  } else {
+    addAndRemoveClases("contador__descanso", "contador__trabajo");
+    SPANTXT.innerHTML = modoTimer;
+
+    if (segundosTotales < 0) {
+      modoTimer = "trabajo";
+      segundosTotales = MINUTOS_DE_TRABAJO * 60;
+    }
+  }
+};
+
+// Funcion para añadir y quitar las clase
+const addAndRemoveClases = (addClase, removeClase) => {
+  CONTAINER.classList.remove(removeClase);
+  CONTAINER.classList.add(addClase);
+};
+
+// RUN
+setInterval(() => cuentaRegresiva(), 1000);
